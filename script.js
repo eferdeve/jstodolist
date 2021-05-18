faf9b466d002e5331c8b955818643897971fda84
Ferres Enzo <Enzo@MacBook-Pro-de-Ferres.local>
2021-05-18 23:00:03 +0200

    localstorage success

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ $(document).ready(function() {
         if (e.code === "Enter") {  
             addItem(e);
         }
-    })
-});
+    });
 
+    // Récupère le localstorage en entier
+    let itemIds = Object.keys(localStorage);
+    let itemVal = Object.values(localStorage);
+
+    for (let i = 0; i < itemIds.length; i++) {
+        $('.todoItemsContainer').append(
+           $('<span class="item"' + ' id="' + itemIds[i] + '">' + itemVal[i] + '</span>'))
+    }
+  
+
+});
 
 function addItem() {
     // Récupère la valeur de l'input
@@ -18,8 +28,17 @@ function addItem() {
     let itemId = 'item_' + itemNumber;
 
     let itemDiv = $('<span class="item"' + ' id="' + itemId + '">' + itemValue + '</span>');
-    
+
+    localStorage.setItem(itemId, itemValue);
+
     $('.todoItemsContainer').append(itemDiv);
     $('#itemInput').val("");
+
     console.log('Item ajouter à la liste avec l\'id : ' + itemId);
 }
+
+// récupérer les valeurs de l'input
+
+// les stocker dans le localstorage
+
+// récupérer les valeurs du localstorage et les afficher dans la div
